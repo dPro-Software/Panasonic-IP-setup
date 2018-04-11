ce3a8c007e01af169e95c2f92283e7dd6b080092
Damiaan Dufaux <damiaan@dpro.be>
2018-04-12 01:28:16 +0200

    Initial Commit

---
+++ Panasonic IP setup/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Panasonic IP setup
+//
+//  Created by Damiaan on 12/04/18.
+//  Copyright © 2018 Devian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Panasonic IP setup/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Panasonic IP setup
+//
+//  Created by Damiaan on 12/04/18.
+//  Copyright © 2018 Devian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
